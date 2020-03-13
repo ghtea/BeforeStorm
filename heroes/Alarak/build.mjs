6b5a8d28f09ceb3674a3db64a30a37f91c802a78
PJH <eiirwp@gmail.com>
2020-03-13 19:55:26 +0900

    q

--- heroes/Alarak/build.mjs
+++ heroes/Alarak/build.mjs
@@ -1,8 +1,53 @@
 /* https://github.com/developit/htm */
 
-import { html, Component, render } from 'https://ghtea.github.io/Countries/common/standalone.module.js';
+import { html, Component, render } from 'https://ghtea.github.io/StormBook/#common/standalone.module.js';
+
+const Build = () => html` 
+<div class="divBuild">
+      <div class="divBuildTitle"> <p> Build 1
+      </p></div>
+      <table>
+      <colgroup>
+          <col class="colS" />
+          <col class="colS" />
+          <col class="colS" />
+          <col class="colS" />
+          <col class="colS" />
+          <col class="colS" />
+          <col class="colS" />
+      </colgroup>
+      
+      <tr>
+      <th>1</th>
+      <th>4</th>
+      <th>7</th>
+      <th>10</th>
+      <th>13</th>
+      <th>16</th>
+      <th>20</th>
+      </tr>
+      <tr class="rowTalent">
+      <td><img src="../../#images/talents/Alarak/04_Show_of_Force.png" class = "imgTalent"></td>
+      <td><img class = "imgTalent"></td>
+      <td><img class = "imgTalent"></td>
+      <td><img class = "imgTalent"></td>
+      <td><img class = "imgTalent"></td>
+      <td><img class = "imgTalent"></td>
+      <td><img class = "imgTalent"></td>
+      </tr>
+   </table>
+  
+   <div class="divWinRate"> 
+      <div>WinRate</div>
+      <div class="barWinRate">  </div>
+      <div> 55.5% </div>
+   </div>
+  
+  </div>
+`;
 
 
+render(html`<${Build}/>`, document.body);
 
 /*
   const CountryName = ({ countryCode }) => html`<h1>${objCountryBasic[countryCode]['Country']}</h1>`;
