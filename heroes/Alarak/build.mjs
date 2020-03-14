432819e2529a76313bfcaee09d96f99ded52f838
PJH <eiirwp@gmail.com>
2020-03-14 13:51:54 +0900

    q

--- heroes/Alarak/build.mjs
+++ heroes/Alarak/build.mjs
@@ -2,11 +2,14 @@
 
 import { html, Component, render } from 'https://ghtea.github.io/StormBook/common/standalone.module.js';
 
-const Build = () => html` 
+const Build = () => html`
 <div class="divBuild">
       <div class="divBuildTitle"> <p> Build 1
       </p></div>
-      <table>
+      
+      
+      <table>  
+      
       <colgroup>
           <col class="colS" />
           <col class="colS" />
@@ -26,15 +29,18 @@ const Build = () => html`
       <th>16</th>
       <th>20</th>
       </tr>
+      
       <tr class="rowTalent">
-      <td><img src="../../images/talents/Alarak/04_Show_of_Force.png" class = "imgTalent"></td>
-      <td><img class = "imgTalent"></td>
-      <td><img class = "imgTalent"></td>
-      <td><img class = "imgTalent"></td>
-      <td><img class = "imgTalent"></td>
-      <td><img class = "imgTalent"></td>
-      <td><img class = "imgTalent"></td>
+         <td><img src="../../images/talents/Alarak/01_Extended_Lightning.png" class="imgTalent"/> </td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+         
       </tr>
+      
    </table>
   
    <div class="divWinRate"> 
@@ -47,42 +53,5 @@ const Build = () => html`
 `;
 
 
-render(html`<${Build}/>`, document.getElelmentById("divBuildAll"));
-
-/*
-  const CountryName = ({ countryCode }) => html`<h1>${objCountryBasic[countryCode]['Country']}</h1>`;
-
-
-
-const CountryFlag = ({ countryCode })=> html`  <img class="imgFlag" src=${"flagsPng/" + objCountryBasic[countryCode]['Alpha2'].toLowerCase()  + ".png"} />`;
-
-
-const CounyryPopChart = ({ countryCode })=> html`
-  <svg width="400" height="200"   class="chart">
-  <polyline
-     fill="none"
-     stroke="#0074d9"
-     stroke-width="2"
-     points="
-       0,0
-       0,${objCountryPop[countryCode]['1950']/400}
-       30,${objCountryPop[countryCode]['1960']/400}
-       60,${objCountryPop[countryCode]['1970']/400}
-       90,${objCountryPop[countryCode]['1980']/400}
-       120,${objCountryPop[countryCode]['1990']/400}
-       150,${objCountryPop[countryCode]['2000']/400}
-       180,${objCountryPop[countryCode]['2010']/400}
-       210,${objCountryPop[countryCode]['2020']/400}
-       "/>
-</svg>
-`;
-
-
-const Table = () => html`
-  <${CountryName} countryCode=${countryCode}/> 
-  <${CountryFlag} countryCode=${countryCode}/>
-  <${CounyryPopChart} countryCode=${countryCode}/>
-  `;
-
-render(html`<${Country}  countryCode="4" />`, document.body);
-*/
+render(html`<${Build}/>`, document.getElementById("divBuildAll"));
+   
---
+++ heroes/Alarak/buildOld.mjs
@@ -0,0 +1,91 @@
+/* https://github.com/developit/htm */
+
+import { html, Component, render } from 'https://ghtea.github.io/StormBook/common/standalone.module.js';
+
+const Build = () => html`
+<div class="divBuild">
+      <div class="divBuildTitle"> <p> Build 1
+      </p></div>
+      <table>  
+      
+      <colgroup>
+          <col class="colS" />
+          <col class="colS" />
+          <col class="colS" />
+          <col class="colS" />
+          <col class="colS" />
+          <col class="colS" />
+          <col class="colS" />
+      </colgroup>
+      
+      <tr>
+      <th>1</th>
+      <th>4</th>
+      <th>7</th>
+      <th>10</th>
+      <th>13</th>
+      <th>16</th>
+      <th>20</th>
+      </tr>
+      
+      <tr class="rowTalent">
+      <td><img src="../../images/talents/Alarak/04_Show_of_Force.png" class = "imgTalent"></td>
+      <td><img src="../../images/talents/Alarak/04_Show_of_Force.png" class = "imgTalent"></td>
+      <td><img src="../../images/talents/Alarak/04_Show_of_Force.png"  class = "imgTalent" ></td>
+      <td><img src="../../images/talents/Alarak/04_Show_of_Force.png"  class = "imgTalent"></td>
+      <td><img src="../../images/talents/Alarak/04_Show_of_Force.png"  class = "imgTalent"></td>
+      <td><img src="../../images/talents/Alarak/04_Show_of_Force.png"  class = "imgTalent"></td>
+      <td><img src="../../images/talents/Alarak/04_Show_of_Force.png"  class = "imgTalent"></td>
+      </tr>
+      
+   </table>
+  
+   <div class="divWinRate"> 
+      <div>WinRate</div>
+      <div class="barWinRate">  </div>
+      <div> 55.5% </div>
+   </div>
+  
+  </div>
+`;
+
+
+render(html`<${Build}/>`, document.getElementById("divBuildAll"));
+
+/*
+  const CountryName = ({ countryCode }) => html`<h1>${objCountryBasic[countryCode]['Country']}</h1>`;
+
+
+
+const CountryFlag = ({ countryCode })=> html`  <img class="imgFlag" src=${"flagsPng/" + objCountryBasic[countryCode]['Alpha2'].toLowerCase()  + ".png"} />`;
+
+
+const CounyryPopChart = ({ countryCode })=> html`
+  <svg width="400" height="200"   class="chart">
+  <polyline
+     fill="none"
+     stroke="#0074d9"
+     stroke-width="2"
+     points="
+       0,0
+       0,${objCountryPop[countryCode]['1950']/400}
+       30,${objCountryPop[countryCode]['1960']/400}
+       60,${objCountryPop[countryCode]['1970']/400}
+       90,${objCountryPop[countryCode]['1980']/400}
+       120,${objCountryPop[countryCode]['1990']/400}
+       150,${objCountryPop[countryCode]['2000']/400}
+       180,${objCountryPop[countryCode]['2010']/400}
+       210,${objCountryPop[countryCode]['2020']/400}
+       "/>
+</svg>
+`;
+
+
+const Table = () => html`
+  <${CountryName} countryCode=${countryCode}/> 
+  <${CountryFlag} countryCode=${countryCode}/>
+  <${CounyryPopChart} countryCode=${countryCode}/>
+  `;
+
+render(html`<${Country}  countryCode="4" />`, document.body);
+*/
