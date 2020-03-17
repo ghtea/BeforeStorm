7db7b5ffbc93e14dd1f4362155f870fd164e5381
PJH <eiirwp@gmail.com>
2020-03-17 18:30:13 +0900

    q

--- 0/mjs/build.mjs
+++ 0/mjs/build.mjs
@@ -1,6 +1,6 @@
 /* https://github.com/developit/htm */
 
-import { html, Component, render, useState } from 'https://ghtea.github.io/StormBook/0/common/standalone.module.js';
+import { html, Component, render, useState } from 'https://ghtea.github.io/BeforeStorm/0/common/standalone.module.js';
 
 /* manipulate data */
 objHeroBuild.sort(function(a, b) { 
